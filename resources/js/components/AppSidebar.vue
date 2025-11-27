1c55979717539a7337d330a27c4eb37865c29f33
Walter Neto <contato.walter@proton.me>
2025-11-26 23:08:01 -0300

    add Contratos, Financeiro, Inquilinos, and Relatorios pages and show them in sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -62,7 +62,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/wscneto',
         icon: Folder,
     },
     {
